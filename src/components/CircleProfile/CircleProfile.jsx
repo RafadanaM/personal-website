3648989d8d73dfdc380cf5b51094ef20c8e8e6c4
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-09-03 22:26:59 +0700

    change circle profile and introduction to tailwind

--- src/components/CircleProfile/CircleProfile.jsx
+++ src/components/CircleProfile/CircleProfile.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import profile from "../../images/profile.jpg";
 
 function CircleProfile({ isVisible }) {
-  return <img className="rounded-full" src={profile} alt="Profile" />;
+  return (
+    <img
+      className={`bg-cover border-2 border-solid border-white rounded-full w-60 ${
+        isVisible ? "opacity-100" : "opacity-0 transform -translate-y-52"
+      }  md:w-72 transition duration-700 `}
+      src={profile}
+      alt="Profile"
+    />
+  );
 }
 
 export default CircleProfile;
--- src/pages/Sections/Introduction/Introduction.jsx
+++ src/pages/Sections/Introduction/Introduction.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import classes from "./Introduction.module.css";
+
 import CircleProfile from "../../../components/CircleProfile/CircleProfile";
 import useOnScreen from "../../../components/UseOnScreen";
 
@@ -7,30 +7,33 @@ function Introduction() {
   const ref = useRef();
   const isVisible = useOnScreen(ref);
   return (
-    <section ref={ref} id="/" className={classes.introductionSection}>
-      <div className={classes.introductionLeft}>
+    <section
+      ref={ref}
+      id="/"
+      className="grid grid-cols-1 grid-rows-2 h-screen w-full md:grid-cols-2 md:grid-rows-1"
+    >
+      <div className="col-start-1 row-start-1 flex p-2 justify-center items-center md:p-8">
         <CircleProfile isVisible={isVisible} />
       </div>
-      <div
-        className={`${classes.introductionRight} ${
-          isVisible && classes.visible
-        }`}
-        style={{ flexGrow: 1 }}
-      >
+      <div className="col-start-1 row-start-2 row-span-2 text-5xl flex flex-col p-2 md:justify-center md:col-start-2 md:row-start-1 text-white md:p-8 md:text-7xl">
         <h1
-          className={`${classes.initialName} ${isVisible && classes.firstname}`}
+          className={`text-center transition duration-1000 delay-150 ${
+            isVisible ? "opacity-100" : "opacity-0 transform -translate-x-96"
+          } md:text-left md:ml-0`}
         >
           MUHAMMAD
         </h1>
         <h1
-          className={`${classes.initialName} ${
-            isVisible && classes.middlename
+          className={`text-center transition duration-1000 delay-150 ${
+            isVisible ? "opacity-100" : "opacity-0 transform translate-x-96"
           }`}
         >
           RAFADANA
         </h1>
         <h1
-          className={`${classes.initialName} ${isVisible && classes.lastname}`}
+          className={`text-center transition duration-1000 delay-150 ${
+            isVisible ? "opacity-100" : "opacity-0 transform -translate-x-96"
+          } md:text-right`}
         >
           MOUNTHEIRA
         </h1>
