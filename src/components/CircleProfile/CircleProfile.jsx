f85b93e3dcc16aa9edc5cbf1f1332cf2fcb2e130
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-09-03 20:56:41 +0700

    change to tailwind

--- src/components/CircleProfile/CircleProfile.jsx
+++ src/components/CircleProfile/CircleProfile.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import classes from "./CircleProfile.module.css";
 import profile from "../../images/profile.jpg";
 
-
-function CircleProfile({isVisible}) {
-  
-  return (
-    <img  className={`${classes.imageDecoration} ${isVisible && classes.appear}`} src={profile} alt="Profile" />
-  );
+function CircleProfile({ isVisible }) {
+  return <img className="rounded-full" src={profile} alt="Profile" />;
 }
 
 export default CircleProfile;
