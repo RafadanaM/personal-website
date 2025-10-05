8e512abfcf6db25464c658debf0e7bc4fb69a8dc
Muhammad Rafadana Mountheira <rafadanam@gmail.com>
2025-10-05 16:55:49 +0700

    fix: mobile nav does not close on navigation

--- src/packages/common/components/Navbar/MobileNavSlider.tsx
+++ src/packages/common/components/Navbar/MobileNavSlider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useCallback } from "react";
 import Link from "next/link";
 
 import AnimatedText from "@/packages/ui/components/AnimatedText";
@@ -7,17 +8,20 @@ import { useNavbar } from "@common/contexts/NavContext";
 import Token from "@/packages/ui/constants";
 
 function MobileNavSlider() {
-  const { isOpen } = useNavbar();
+  const { isOpen, setIsOpen } = useNavbar();
+
+  const handleNavPress = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
 
   return (
     <div
-      aria-hidden={isOpen ? "true" : "false"}
       className={`md:hidden p-4 fixed top-16 z-10 left-0 h-[calc(100vh-4rem)] w-screen bg-secondary transition-transform ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
     >
-      <nav aria-label="primary">
+      <nav aria-label="secondary">
         <ul className="flex flex-col gap-y-1 ">
           <li>
-            <Link href="#home">
+            <Link href="#home" onClick={handleNavPress}>
               <AnimatedText
                 text="HOME"
                 color={Token.colors.neutralDark}
@@ -27,7 +31,7 @@ function MobileNavSlider() {
           </li>
 
           <li>
-            <Link href="#about">
+            <Link href="#about" onClick={handleNavPress}>
               <AnimatedText
                 text="ABOUT"
                 color={Token.colors.neutralDark}
@@ -36,7 +40,7 @@ function MobileNavSlider() {
             </Link>
           </li>
           <li>
-            <Link href="#career">
+            <Link href="#career" onClick={handleNavPress}>
               <AnimatedText
                 text="CAREER"
                 color={Token.colors.neutralDark}
@@ -45,7 +49,7 @@ function MobileNavSlider() {
             </Link>
           </li>
           <li>
-            <Link href="#projects">
+            <Link href="#projects" onClick={handleNavPress}>
               <AnimatedText
                 text="PROJECTS"
                 color={Token.colors.neutralDark}
@@ -54,7 +58,7 @@ function MobileNavSlider() {
             </Link>
           </li>
           <li>
-            <Link href="#contact">
+            <Link href="#contact" onClick={handleNavPress}>
               <AnimatedText
                 text="CONTACT"
                 color={Token.colors.neutralDark}
