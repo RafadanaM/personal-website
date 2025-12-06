0cb153b7b3b2eb8a5c7301209d333431048f0d1d
Muhammad Rafadana Mountheira <rafadanam@gmail.com>
2025-12-06 08:23:56 +0700

    feat: use next font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { PropsWithChildren } from "react";
-
+import localFont from "next/font/local";
 export const metadata: Metadata = {
   title: "Muhammad Rafadana Mountheira",
   description:
@@ -31,17 +31,37 @@ export const metadata: Metadata = {
   keywords: ["Rafadana", "Muhammad Rafadana Mountheira", "Rafa"],
   robots: { index: true, follow: true },
 };
-
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   themeColor: "#0a0f1e",
   colorScheme: "normal",
 };
-
+const font = localFont({
+  src: [
+    {
+      path: "../../public/fonts/ITC Avant Garde Gothic CE Book.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ITC Avant Garde Gothic Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ITC Avant Garde Gothic Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "---font-itc-avant-garde",
+  display: "swap",
+  preload: true,
+});
 export default function IndexLayout({ children }: PropsWithChildren<{}>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${font.className} antialiased`}>
       <body>{children}</body>
     </html>
   );
